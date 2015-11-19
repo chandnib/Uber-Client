ff07f33fae65bc9015b4975af0063f63d59b588f
rominoushana <rominoushana@gmail.com>
2015-11-19 02:36:32 -0800

    cleaned up code

--- routes/customer.js
+++ routes/customer.js
@@ -7,7 +7,7 @@ exports.customerLoginPage = function(req, res){
 	if(req.session.passport != null && req.session.passport != ""){
 		console.log("Existing Session on Passport !!! ==>  " + JSON.stringify(req.session));
 		if(req.session.passport.user.EMAIL != "" && req.session.passport.user.EMAIL != null){
-			res.render('adminHome', {user:JSON.stringify(req.session.passport.user)});
+			res.render('customerHome', {user:JSON.stringify(req.session.passport.user)});
 		}
 		else
 			res.render('loginCustomer', {user:JSON.stringify({})});
@@ -20,7 +20,7 @@ exports.adminHome = function(req, res){
 	console.log("Session Set by Passport !!! ==>  " + JSON.stringify(req.session));
 	if(req.session.passport != null && req.session.passport != ""){
 		if(req.session.passport.user.EMAIL != "" && req.session.passport.user.EMAIL != null){
-			res.render('adminHome', {user:JSON.stringify(req.session.passport.user)});
+			res.render('customerHome', {user:JSON.stringify(req.session.passport.user)});
 		}
 		else
 			res.redirect("/invalidAdminLogin");
